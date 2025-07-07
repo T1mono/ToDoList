bc4cfdd72fd3c40e14efeb1906979c1e9fd40dab
T1mon <godropopka@gmail.com>
2025-07-07 09:38:39 +0500

    Added methods filterTaskByStatus

--- src/main/java/ru/javadaddy/repository/TaskRepositoryImpl.java
+++ src/main/java/ru/javadaddy/repository/TaskRepositoryImpl.java
@@ -1,23 +1,28 @@
 package ru.javadaddy.repository;
 
 import lombok.extern.slf4j.Slf4j;
+import ru.javadaddy.enums.Status;
 import ru.javadaddy.model.Task;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
 public class TaskRepositoryImpl {
     List<Task> tasksRepository = new ArrayList<>();
 
+    Task task = new Task();
+
+    //Создание задачи
     public void createTask(Task task) {
         if (task == null) {
             throw new IllegalArgumentException("Task не может быть равен null");
         }
 
         if (task.getNameTask() == null || task.getNameTask().isBlank()) {
-            throw new IllegalArgumentException("Имя задачи не можеть пустым");
+            throw new IllegalArgumentException("Имя задачи не может пустым");
         }
 
         if (task.getPeriodOfExecution() != null && task.getPeriodOfExecution().isBefore(LocalDate.now())) {
@@ -25,5 +30,41 @@ public class TaskRepositoryImpl {
         }
 
         tasksRepository.add(task);
+    }
+
+    //Получить список задач
+    public List<Task> getTasks() {
+        if (tasksRepository == null || tasksRepository.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return new ArrayList<>(tasksRepository);
+    }
+
+    //Удалить задачу
+    public void removeTask(Long id) {
+
+        if (id == null) {
+            throw new IllegalArgumentException("ID не может быть null");
+        }
+
+        tasksRepository.removeIf(i -> i.getId().equals(id));
+    }
+
+    //Фильтровать задачи по статусу
+    public List<Task> filterTaskByStatus(Status status) {
+
+        List<Task> result = new ArrayList<>();
+
+        if (status == null){
+            throw new IllegalArgumentException("Статус не может быть null");
+        }
+
+        for (Task item : tasksRepository) {
+            if (item.getStatus().equals(status)) {
+                result.add(item);
+            }
+        }
+        return result;
     }
 }
